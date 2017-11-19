7b8dc5dce7b6197feb6941be237b4721e2ae257b
Adrian Szulc <Adrian.Bruno.Szulc@gmail.com>
2017-11-19 16:35:48 +0100

    Front-End (without errors, tests and css)

--- DirectWeather/DirectWeather.FrontEnd/ClientApp/components/Home.tsx
+++ DirectWeather/DirectWeather.FrontEnd/ClientApp/components/Home.tsx
@@ -1,10 +1,69 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import 'isomorphic-fetch';
+import { WeatherOutput } from "./WeatherOutput";
 
-export class Home extends React.Component<RouteComponentProps<{}>, {}> {
+
+interface SearchDataState {
+    country: string;
+    city: string;
+    isVisible : boolean
+    weather: Weather;
+}
+
+export class Weather {
+    Location: { Country: string, City: string };
+    Temperature: { Format: string, Value: number };
+    Humidity: number;
+
+    constructor() {
+        this.Location = { Country: "", City: "" };
+        this.Temperature = { Format: "", Value: 0 },
+            this.Humidity = 0;
+    }
+}
+
+export class Home extends React.Component<RouteComponentProps<{}>, SearchDataState> {
+    constructor(props: any) {
+        super(props);
+        this.state = { country: "", city: "", isVisible : false, weather: new Weather() };
+
+        this.setCountry = this.setCountry.bind(this);
+        this.setCity = this.setCity.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    setCountry(e: any) {
+        this.setState({ country: e.target.value });
+    }
+
+    setCity(e: any) {
+        this.setState({ city: e.target.value });
+    }
+
+    handleSubmit(event: any) {
+        event.preventDefault();
+        this.searchCommand(this.state.country, this.state.city);
+    }
+
+    searchCommand(country: string, city: string) {
+        this.setState({ isVisible: false});
+        fetch("http://localhost:5268/api/Weather/" + country + "/" + city)
+            .then(response => response.json() as Promise<Weather>)
+            .then(data => {
+                this.setState({ weather: data });
+                this.setState({ isVisible: true });
+            });
+    }
+
     public render() {
         return <div>
-            <h1>Hello, world!</h1>
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="Country:" type="text" value={this.state.country} name="country" onChange={this.setCountry} />
+                <input placeholder="City:" type="text" value={this.state.city} name="city" onChange={this.setCity} />
+                <input type="submit" value="Submit" />
+            </form>
+            <WeatherOutput weather={this.state.weather} isVisible={this.state.isVisible}/>
         </div>;
     }
 }
---
+++ DirectWeather/DirectWeather.FrontEnd/ClientApp/components/WeatherOutput.tsx
@@ -0,0 +1,27 @@
+﻿import * as React from 'react';
+import * as Home from "./Home";
+import Weather = Home.Weather;
+
+interface WeatherProps {
+    weather: Weather,
+    isVisible : boolean;
+}
+
+
+export class WeatherOutput extends React.Component<WeatherProps, {}> {
+    render() {
+        if (this.props.isVisible) {
+            return (
+                <div>
+                    <h3>Weather for:</h3>
+                    <p>Country: {this.props.weather.Location.Country}</p>
+                    <p>City: {this.props.weather.Location.City}</p>
+                    <p>Humidity: {this.props.weather.Humidity}</p>
+                    <p>Teperature: {this.props.weather.Temperature.Value} {this.props.weather.Temperature.Format}</p>
+                </div>
+            );
+        } else {
+            return <div/>;
+        }
+    }
+}
